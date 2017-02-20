d1d869851af3fd77368ee6ac2b2b383f15fe1119
AleksandraSiedlarczyk <aleksandra.siedlarczyk@gmail.com>
2017-02-20 21:11:14 +0100

    Form Authentication test start

---
+++ Herokuapp.Automated.Tests/Herokuapp.Pages/FormAuthPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herokuapp.Pages
+{
+    public class FormAuthPage : BasePage
+    {
+        public FormAuthPage(IWebDriver driver) : base(driver)
+        {
+        }
+    }
+}
